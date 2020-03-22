750a8b2412a303d00704b454128b9daf946f16f8
naimishverma17 <itakgec@gmail.com>
2020-03-22 19:31:18 +0530

    added first endpoint

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+
+class HelloAPIView(APIView):
+    """Custom designed api end points"""
+
+    def get(self, request, format=None):
+        return Response({'message': 'Hello'})
