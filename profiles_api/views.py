cf9ea5b26c938e81d684c3239946f8ad6695a607
naimishverma17 <itakgec@gmail.com>
2020-03-22 22:28:13 +0530

    added first serializer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 # Create your views here.
@@ -9,5 +12,18 @@ from rest_framework.response import Response
 class HelloAPIView(APIView):
     """Custom designed api end points"""
 
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         return Response({'message': 'Hello'})
+
+    def post(self, request):
+        """Create a greeting with name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            greeting = f'Hello {name}'
+            return Response({'message': greeting})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
